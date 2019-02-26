fcac7c8b705c425dd1bc2ce83d2cc21e17e2d0d4
louisroman <louisroman133@gmail.com>
2019-02-26 15:31:40 -0500

    MVP

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,16 +56,33 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+
+runners.forEach(function(currentValue){
+    fullName.push(currentValue.first_name + " " + currentValue.last_name);
+})
 console.log(fullName);
+
+
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+
+runners.map(function(currentValue){
+    allCaps.push(currentValue.first_name.toUpperCase());
+})
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
-// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
+// The large shirts won't be available for the event due to an ordering issue.  
+// Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that 
+// contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+
+largeShirts = runners.filter(function(currentValue){
+    return currentValue.shirt_size == "L";
+})
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
@@ -74,10 +91,36 @@ let ticketPriceTotal = [];
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce().  
+// I want you to think of potential problems you could solve given the data set 
+// and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many 
+// of the array methods listed above.
 
-// Problem 1
+// Problem 1: alphabetize by last name
 
-// Problem 2
+let alphaLast = [];
 
-// Problem 3
+runners.forEach(function(currentValue){
+    alphaLast.push(currentValue.last_name);
+})
+alphaLast.sort();
+
+console.log(alphaLast);
+
+// Problem 2: add total donation
+
+const donationTot = runners.reduce((accumulator,currentValue) => {
+
+    return accumulator + currentValue.donation;
+
+}, 0); 
+
+console.log(donationTot);
+
+// Problem 3: Grab all the emails
+
+const runnerEmails = runners.map(function(currentValue){
+    return currentValue.email;
+})
+
+console.log(runnerEmails);
--- assignments/closure.js
+++ assignments/closure.js
@@ -2,20 +2,32 @@
 // Write a simple closure of your own creation.  Keep it simple!
 
 
-/* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
+let counter = 0;
+
+let add = (function () {
+  return function () {counter += 1; return counter}
+})();
+
+add();
+add();
+add();
+
+console.log(counter);
+
+// /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
-// ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// // ==== Challenge 2: Create a counter function ====
+// const counter = () => {
+//   // Return a function that when invoked increments and returns a counter variable.
+// };
+// // Example usage: const newCounter = counter();
+// // newCounter(); // 1
+// // newCounter(); // 2
 
-// ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+// // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// const counterFactory = () => {
+//   // Return an object that has two methods called `increment` and `decrement`.
+//   // `increment` should increment a counter variable in closure scope and return it.
+//   // `decrement` should decrement the counter variable and return it.
+// };
